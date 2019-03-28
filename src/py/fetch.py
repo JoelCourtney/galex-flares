92a9af326700f4ed050f34d64537a3b65e13fb2c
Joel Courtney <joelcourtney@Puma-617.local>
2019-03-28 02:44:47 -0600

    info fetched

--- src/py/fetch.py
+++ src/py/fetch.py
@@ -12,71 +12,98 @@ class NumpyEncoder(json.JSONEncoder):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
-def info(star):
-    dir = 'data/stars/' + star['ID']
-    if os.path.exists(dir):
-        shutil.rmtree(dir)
-    os.mkdir(dir)
+def fetch_info(star):
     print('Fetching info for ' + star['ID'])
-    info = gPhoton.gFind(band='NUV', skypos=[star['RAdeg'],star['DEdeg']])
-    with open(dir + '/info.json', 'w') as fp:
-        json.dump(info, fp, cls=NumpyEncoder)
-    return info
-
-def bins(star):
+    info = gPhoton.gFind(band='both',
+        skypos=[star['RAdeg'],star['DEdeg']],
+        exponly=True, verbose=2)
+    info['flare'] = {
+        'reject': 0,
+        't_quiesent': -1,
+        't_start': -1,
+        't_end': -1
+    }
+    info['aperture'] = {
+        'rad': 0.0045,
+        'ann': [0.005,0.006]
+    }
+    write_info(star, info)
+def read_info(star):
     infoFile = 'data/stars/' + star['ID'] + '/info.json'
     if not os.path.exists(infoFile):
-        info(star)
-    infoList = []
+        fetch_info(star)
     with open(infoFile) as json_file:
-        infoList = json.load(json_file)
-    infoList = infoList['NUV']
-    dataFile = 'data/stars/' + star['ID'] + '/bins.csv'
-    print('Fetching: ' + star['ID'])
-    print('Fetching bins for ' + star['ID'])
-    gPhoton.gAperture(band='NUV', skypos=[star['RAdeg'],star['DEdeg']],
-        csvfile=dataFile, radius=0.012,
-        annulus=[0.013,0.016], verbose=2)
+        info = json.load(json_file)
+        return info
+def write_info(star, info):
+    dir = 'data/stars/' + star['ID']
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    with open(dir + '/info.json', 'w') as fp:
+        json.dump(info, fp, cls=NumpyEncoder, indent=4)
 
-def window(star):
-    binsFile = 'data/stars/' + star['ID'] + '/bins.csv'
-    if not os.path.exists(binsFile):
-        bins(star)
-    binsList = pd.read_csv(binsFile)
-    # print(len(bins))
-    # bins = bins.loc[bins['flux_mcatbgsub']!=bins['flux_mcatbgsub'].max()]
-    # print(len(bins))
-    flare = binsList.loc[binsList['flux_mcatbgsub'].idxmax()]
+def fetch_exposures(star):
+    info = read_info(star)
+    expsFile = 'data/stars/' + star['ID'] + '/exposures.csv'
+    print('Fetching exposures for ' + star['ID'])
+    gPhoton.gAperture(band='NUV', skypos=info['NUV']['nearest_source']['skypos'],
+        csvfile=expsFile, radius=info['aperture']['rad'],
+        annulus=info['aperture']['ann'], verbose=2)
+def read_exposures(star):
+    expsFile = 'data/stars/' + star['ID'] + '/exposures.csv'
+    if not os.path.exists(expsFile):
+        fetch_exposures(star)
+    return pd.read_csv(expsFile)
+
+def fetch_window(star):
+    exps = read_exposures(star)
+    info = read_info(star)
+    for _ in range(info['flare']['reject']):
+        exps = exps.loc[exps['flux_mcatbgsub']!=exps['flux_mcatbgsub'].max()]
+    flare = exps.loc[exps['flux_mcatbgsub'].idxmax()]
     windowFile = 'data/stars/' + star['ID'] + '/window.csv'
     print('Fetching brightest window for ' + star['ID'])
-    gPhoton.gAperture(band='NUV', skypos=[star['RAdeg'],star['DEdeg']], stepsz=10.,
-        csvfile=windowFile, radius=0.012,
-        annulus=[0.013,0.016], verbose=2,
+    gPhoton.gAperture(band='NUV', skypos=info['NUV']['nearest_source']['skypos'], stepsz=30.,
+        csvfile=windowFile, radius=info['aperture']['rad'],
+        annulus=info['aperture']['ann'], verbose=2,
         trange=[flare['t0'],flare['t1']])
-
-def flare(star):
-    print('Please delimit flare:')
+def read_window(star):
     windowFile = 'data/stars/' + star['ID'] + '/window.csv'
     if not os.path.exists(windowFile):
-        fetch.window(star)
-    flare = pd.read_csv(windowFile)
-    fig = flare.plot(x="t_mean",y="flux_mcatbgsub").get_figure()
-    coords = []
+        fetch_window(star)
+    return pd.read_csv(windowFile)
+
+def fetch_flare(star):
+    window = read_window(star)
+    fig = window.plot(x="t_mean",y="flux_mcatbgsub").get_figure()
+    times = []
     def onclick(event):
         global ix, iy
         ix, iy = event.xdata, event.ydata
         print('x = %d, y = %d'%(
             ix, iy))
-        coords.append(ix)
-        if len(coords) == 2:
+        times.append(round(ix))
+        if len(times) == 3:
             fig.canvas.mpl_disconnect(cid)
             plt.close(fig)
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    print('Please delimit flare for ' + star['ID'])
     plt.show()
+
+    info = read_info(star)
+    info['flare']['t_quiesent'] = times[0]
+    info['flare']['t_start'] = times[1]
+    info['flare']['t_end'] = times[2]
+    write_info(star, info)
 
     flareFile = 'data/stars/' + star['ID'] + '/flare.csv'
     print('Fetching flare for ' + star['ID'])
-    gPhoton.gAperture(band='NUV', skypos=[star['RAdeg'],star['DEdeg']], stepsz=2.,
-        csvfile=flareFile, radius=0.012,
-        annulus=[0.013,0.016], verbose=2,
-        trange=[round(coords[0]),round(coords[1])])
+    gPhoton.gAperture(band='NUV', skypos=info['NUV']['nearest_source']['skypos'], stepsz=2.,
+        csvfile=flareFile, radius=info['aperture']['rad'],
+        annulus=info['aperture']['ann'], verbose=2,
+        trange=times[0:1])
+def read_flare(star):
+    flareFile = 'data/stars/' + star['ID'] + '/flare.csv'
+    if not os.path.exists(flareFile):
+        fetch_flare(star)
+    return pd.read_csv(flareFile)
--- src/py/main.py
+++ src/py/main.py
@@ -7,26 +7,63 @@ import os
 import shutil
 import json
 import fetch
+import sys
+import time
 
 def plotFlare(star, cropped):
-    flareFile = ''
+    flare = 0;
     if cropped:
-        flareFile = 'data/stars/' + star['ID'] + '/flare.csv'
-        if not os.path.exists(flareFile):
-            fetch.flare(star)
+        flare = fetch.read_flare(star)
     else:
-        flareFile = 'data/stars/' + star['ID'] + '/window.csv'
-        if not os.path.exists(flareFile):
-            fetch.window(star)
-    flare = pd.read_csv(flareFile)
+        flare = fetch.read_window(star)
     flare.plot(x="t0",y="flux_mcatbgsub")
     plt.show()
 
 def main():
     stars = pd.read_csv("data/gezari_clean.csv")
-    star = stars.iloc[1].to_dict()
-    plotFlare(star,True)
-    # fetch.flare(star)
+    for i in range(53):
+        star = stars.iloc[i].to_dict()
+        fetch.read_info(star)
+        fetch.read_exposures(star)
+        plotFlare(star, False)
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 1:
+        main()
+    elif sys.argv[1] == 'info':
+        stars = pd.read_csv("data/gezari_clean.csv")
+        for i in range(53):
+            star = stars.iloc[i].to_dict()
+            fetch.read_info(star)
+    elif sys.argv[1] == 'exps':
+        stars = pd.read_csv("data/gezari_clean.csv")
+        for i in range(53):
+            star = stars.iloc[i].to_dict()
+            fetch.read_exposures(star)
+    elif sys.argv[1] == 'info_backward':
+        stars = pd.read_csv("data/gezari_clean.csv")
+        for i in reversed(range(53)):
+            star = stars.iloc[i].to_dict()
+            fetch.read_info(star)
+    elif sys.argv[1] == 'exps_backward':
+        stars = pd.read_csv("data/gezari_clean.csv")
+        for i in reversed(range(53)):
+            star = stars.iloc[i].to_dict()
+            fetch.read_exposures(star)
+    elif sys.argv[1] == 'fix_info':
+        stars = pd.read_csv("data/gezari_clean.csv")
+        while True:
+            for i in range(53):
+                star = stars.iloc[i].to_dict()
+                infoFile = 'data/stars/' + star['ID'] + '/info.json'
+                if os.path.exists(infoFile):
+                    info = fetch.read_info(star)
+                    if 'aperture' not in info:
+                        print('Fixing info for ' + star['ID'])
+                        info['aperture'] = {
+                            'rad': 0.0045,
+                            'ann': [0.005,0.006]
+                        }
+                        fetch.write_info(star, info)
+            print('Sleeping')
+            time.sleep(30)
